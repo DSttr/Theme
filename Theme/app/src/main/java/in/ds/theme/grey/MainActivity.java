776ee8ddbe09a33bbcded4fd1bff73c8ddf9255b
DSttr <dikisatria10@gmail.com>
2016-05-01 17:26:01 +0700

    Opening project

---
+++ Theme/app/src/main/java/in/ds/theme/grey/MainActivity.java
@@ -0,0 +1,14 @@
+package in.ds.theme.grey;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
